b17d1c2d0ef4a7d60eec456abfe93473c0afb95b
Ashish <ashish12345bisht@gmail.com>
2021-10-29 11:49:00 +0530

    first commit

---
+++ test.js
@@ -0,0 +1,11 @@
+console.log("welcome to js one program")
+var a=2;
+console.log(a);
+function test(){
+    console.log("from function",a);
+    let d=2;
+}
+// console.log(d);
+console.log(a);
+a=3;
+console.log(a);
